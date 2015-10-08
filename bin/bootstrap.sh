9393bcd6bf3dd61aa99371cd2e0dca381a66d911
Wyatt Kirby <kirby.wa@gmail.com>
2015-10-07 21:26:39 -0700

    Add installing submodules to the bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,24 +3,30 @@
 
 # Switch to current directory
 cd "${0%/*}"
+cd ..
+
+# Initialize git submodules
+echo "Installing WordPress..."
+git pull && git submodule init && git submodule update && git submodule status
+echo "Done."
 
 # Clean Up if We've Bootstrapped Before
 echo "Cleaning up existing config..."
-rm ../config/salts.php && rm ../config/local-config.php && rm ../config/remote-config.php
+rm config/salts.php && rm config/local-config.php && rm config/remote-config.php
 echo "Done."
 
 # Grab Salts with Curl
 echo "Generating salts from Wordpress.org..."
-printf "<?php %s\n" >> ../config/salts.php
-curl -s https://api.wordpress.org/secret-key/1.1/salt >> ../config/salts.php
+printf "<?php %s\n" >> config/salts.php
+curl -s https://api.wordpress.org/secret-key/1.1/salt >> config/salts.php
 echo "Done."
 
 # Copy Local Config
 echo "Creating local config file..."
-cp -a ../config/local-config-sample.php ../config/local-config.php
+cp -a config/local-config-sample.php config/local-config.php
 echo "Done."
 
 # Create Shared Directory
 echo "Creating shared uploads directory..."
-mkdir -p ../shared/uploads
+mkdir -p shared/uploads
 echo "Done."
